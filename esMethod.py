11c64fa728c140c12a43a9a1d6f6d9c250a96796
galvinleow <47635075+galvinleow@users.noreply.github.com>
2020-07-23 21:50:07 +0800

    remove elasticsearch

--- esMethod.py
+++
@@ -1,6 +0,0 @@
-from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search, Q
-
-# def index(client, index, json_dict):
-#     client.create(index, "_doc", json_dict, refresh=True)
-#     print('test success')

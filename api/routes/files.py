5d8055b6587caedc310e38f8a13fc7bdd69f7667
limyingying2000 <65782648+limyingying2000@users.noreply.github.com>
2021-12-19 17:56:21 +0800

    Add registration&stations files.py

--- api/routes/files.py
+++ api/routes/files.py
@@ -25,3 +25,25 @@ def get_questions_csv():
     res = Response(buf.getvalue(), status=200, mimetype="text/csv")
     res.headers.set("Content-Disposition", "attachment", filename="questions.csv")
     return res
+
+
+@app.route("/registration.csv", methods=["GET"])
+def get_registration_csv():
+    buf = StringIO()
+    with db.getconn() as conn:
+        copy_query_to_file("registration", buf, conn)
+        buf.flush() 
+    res = Response(buf.getvalue(), status=200, mimetype="text/csv")
+    res.headers.set("Content-Disposition", "attachment", filename="registration.csv")
+    return res
+
+
+@app.route("/stations.csv", methods=["GET"])
+def get_stations_csv():
+    buf = StringIO()
+    with db.getconn() as conn:
+        copy_query_to_file("station", buf, conn)
+        buf.flush() 
+    res = Response(buf.getvalue(), status=200, mimetype="text/csv")
+    res.headers.set("Content-Disposition", "attachment", filename="stations.csv")
+    return res
